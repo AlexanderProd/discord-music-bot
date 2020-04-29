9caf86b7644d374c53e280c914c84c040f6398ba
Alexander Hörl <alexander.horl2@live.de>
2020-04-29 09:37:48 +0200

    added meme command

---
+++ commands/meme.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const f = require('../functions.js');
+
+module.exports = {
+  name: 'meme',
+  description:
+    "I'm gonna show you a dank meme. You can also select a language, available are de, en, es, it, ru, fr.",
+  usage: '<empty or one of en, de, ru, it, fr>',
+  async execute(message, args) {
+    console.log(f.t() + message.author.tag + ` used the ${this.name} command`);
+
+    const lang =
+      args[0] === 'de' || 'en' || 'fr' || 'ru' || 'es' || 'it'
+        ? `?lang=${args[0]}`
+        : null;
+
+    request(
+      `https://memes.blademaker.tv/api${lang}`,
+      { json: true },
+      (err, res, body) => {
+        if (err) {
+          return console.log(err);
+        }
+
+        const { image, title } = body;
+
+        message.channel.send(`**${title}**\n${image}`);
+      }
+    );
+  },
+};
